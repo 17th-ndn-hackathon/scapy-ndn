2fb645c4d7baa286cbb1774dee0c70916520195a
Ashlesh Gawande <git@ashlesh.me>
2020-11-14 18:50:41 -0800

    initial commit

--- ndn.py
+++ ndn.py
@@ -1,7 +1,9 @@
-from scapy.all import Field, Packet, XByteField, StrLenField, struct
+import struct
+
+from scapy.all import Field, Packet, XByteField, StrLenField, PacketListField, conf
 
 class NdnLenField(Field):
-    __slots__ = ["length_of"]
+    __slots__ = [ "length_of" ]
 
     def __init__(self, name, default, length_of, fmt="!H"):  # noqa: E501
         Field.__init__(self, name, default, fmt)
@@ -10,31 +12,40 @@ class NdnLenField(Field):
     def i2m(self, pkt, x):
         if x is None:
             fld, fval = pkt.getfield_and_val(self.length_of)
-            #print(fld, fval)
             x = fld.i2len(pkt, fval)
-            #print(x)
+        #elif isinstance(x, str):
+        #    return bytes_encode(x)
         return x
 
     def addfield(self, pkt, s, val):
         x = self.i2m(pkt, val)
-        print(x, type(x), val, pkt, s)
         if x < 253:
-            return s + struct.pack("1s", x.to_bytes(1, byteorder='big'))
+            return s + struct.pack(">B", x)
         elif x < 65536:
-            return s + b"\xFD" + struct.pack("2s", x.to_bytes(2, byteorder='big'))
+            return s + b"\xFD" + struct.pack(">H", x)
         elif x < 4294967296:
-            return s + b"\xFE" + struct.pack("4s", x.to_bytes(4, byteorder='big'))
+            return s + b"\xFE" + struct.pack(">L", x)
         else:
-            return s + b"\xFF" + struct.pack("8s", x.to_bytes(8, byteorder='big'))
+            return s + b"\xFF" + struct.pack(">Q", x)
 
     def getfield(self, pkt, s):
-        return s[self.sz:], self.m2i(pkt, self.struct.unpack(s[:self.sz])[0])
+        x = ord(s[:self.sz - 1])
+        if x < 253:
+            return s[1:], self.m2i(pkt, struct.unpack(">B", s[:1])[0])
+        elif x < 65536:
+            return s[3:], self.m2i(pkt, struct.unpack(">H", s[:2])[0])
+        elif x < 4294967296:
+            return s[5:], self.m2i(pkt, struct.unpack(">L", s[:4])[0])
+        else:
+            return s[7:], self.m2i(pkt, struct.unpack(">Q", s[:8])[0])
+
+TYPES = { 'Name': 0x07, 'GenericNameComponent': 0x08 }
 
 class NameComponent(Packet):
     name = "Name Component"
 
     fields_desc = [
-                    XByteField("type", 0x008),
+                    XByteField("type", TYPES['GenericNameComponent']),
                     NdnLenField("length", None, length_of="value"),
                     StrLenField("value", "", length_from=lambda pkt: pkt.length)
                   ]
@@ -46,7 +57,13 @@ class Name(Packet):
     name = "Name"
 
     fields_desc = [
-                    XByteField("type", 0x008),
+                    XByteField("type", TYPES['Name']),
                     NdnLenField("length", None, length_of="value"),
-                    StrLenField("value", "", length_from=lambda pkt: pkt.length)
+                    PacketListField("value", [],
+                                    NameComponent,
+                                    count_from=lambda pkt : Name.count_components(pkt))
                   ]
+
+    def count_components(pkt):
+        print("sjvdbjhsb")
+        return 2
