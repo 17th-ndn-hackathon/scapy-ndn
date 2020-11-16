e5a0f8afed25a832bac6b62f49473a9e4790f6b5
Ashlesh Gawande <git@ashlesh.me>
2020-11-15 19:36:40 -0800

    misc enhancements

--- ndn.py
+++ ndn.py
@@ -1,18 +1,29 @@
 import struct
 
-from scapy.all import Field, Packet, XByteField, StrLenField, PacketListField, conf
+from scapy.all import Field, Packet, XByteField, StrLenField, \
+                      PacketListField, conf, StrFixedLenField
+
+TYPES = {
+          'ImplicitSha256DigestComponent': 0x01,
+          'ParametersSha256DigestComponent': 0x02,
+          'Name': 0x07,
+          'GenericNameComponent': 0x08
+        }
 
 class NdnLenField(Field):
-    __slots__ = [ "length_of" ]
 
-    def __init__(self, name, default, length_of, fmt="!H"):  # noqa: E501
+    def __init__(self, name, default, fmt="!H"):  # noqa: E501
         Field.__init__(self, name, default, fmt)
-        self.length_of = length_of
 
     def i2m(self, pkt, x):
         if x is None:
-            fld, fval = pkt.getfield_and_val(self.length_of)
-            x = fld.i2len(pkt, fval)
+            for field in pkt.fields_desc:
+                if field.name != "type" and field.name != "length":
+                    fld, fval = pkt.getfield_and_val(field.name)
+                    if x is None:
+                        x = fld.i2len(pkt, fval)
+                    else:
+                        x += fld.i2len(pkt, fval)
         #elif isinstance(x, str):
         #    return bytes_encode(x)
         return x
@@ -39,26 +50,52 @@ class NdnLenField(Field):
         else:
             return s[7:], self.m2i(pkt, struct.unpack(">Q", s[:8])[0])
 
-TYPES = { 'Name': 0x07, 'GenericNameComponent': 0x08 }
+class NdnTypeField(NdnLenField):
+    def __init__(self, name, default, fmt="!H"):  # noqa: E501
+        NdnLenField.__init__(self, name, default, fmt)
+
+    def i2m(self, pkt, x):
+        if x is None:
+            fld, fval = pkt.getfield_and_val(self.name)
+            if x is None:
+                x = fld.i2len(pkt, fval)
+        return x
 
 class NameComponent(Packet):
     name = "Name Component"
 
     fields_desc = [
-                    XByteField("type", TYPES['GenericNameComponent']),
-                    NdnLenField("length", None, length_of="value"),
+                    NdnTypeField("type", TYPES['GenericNameComponent']),
+                    NdnLenField("length", None),
                     StrLenField("value", "", length_from=lambda pkt: pkt.length)
                   ]
 
     def guess_payload_class(self, p):
         return conf.padding_layer
 
+# Following two classes given for convenience:
+class ImplicitSha256DigestComponent(NameComponent):
+
+    fields_desc = [
+                    XByteField("type", TYPES['ImplicitSha256DigestComponent']),
+                    XByteField("length", 32),
+                    StrFixedLenField("value", "", 32)
+                  ]
+
+class ParametersSha256DigestComponent(NameComponent):
+
+    fields_desc = [
+                    XByteField("type", TYPES['ParametersSha256DigestComponent']),
+                    XByteField("length", 32),
+                    StrFixedLenField("value", "", 32)
+                  ]
+
 class Name(Packet):
     name = "Name"
 
     fields_desc = [
                     XByteField("type", TYPES['Name']),
-                    NdnLenField("length", None, length_of="value"),
+                    NdnLenField("length", None),
                     PacketListField("value", [],
                                     NameComponent,
                                     count_from=lambda pkt : Name.count_components(pkt))
