ee5f774cc3925eadf16cac7a7ade4888939ad81c
Ashlesh Gawande <git@ashlesh.me>
2020-12-07 23:01:36 -0500

    Adding from_escaped_string

--- ndn.py
+++ ndn.py
@@ -1,8 +1,8 @@
 import struct
 
-from scapy.all import Field, Packet, XByteField, StrLenField, \
+from scapy.all import Field, Packet, XByteField, StrField, StrLenField, \
                       PacketListField, conf, StrFixedLenField, \
-                      PacketField
+                      PacketField, XIntField
 
 TYPES = {
           'ImplicitSha256DigestComponent': 0x01,
@@ -12,6 +12,7 @@ TYPES = {
           'GenericNameComponent': 0x08,
           'CanBePrefix': 0x21, # 33
           'MustBeFresh': 0x12, # 18
+          'Nonce': 0x0a,
         }
 
 class NdnLenField(Field):
@@ -78,27 +79,106 @@ class NameComponent(Packet):
 
     fields_desc = [
                     NdnTypeField(TYPES['GenericNameComponent']),
+                    # Don't need to give it "value" field because NdnLenField computes length
+                    # over all the fields except itself and type field above
                     NdnLenField(),
-                    StrLenField("value", "test", length_from=lambda pkt: pkt.length)
+                    # Packet's length field is determined by NdnLenField above and used by value below
+                    # Otherwise packet boundaries are not determined correctly and padding will be
+                    # merged into the value field
+                    # (TODO: How to/Need to do this for Interest field?)
+                    StrLenField("value", "", length_from=lambda pkt: pkt.length)
                   ]
 
     def guess_payload_class(self, p):
         return conf.padding_layer
 
+    @staticmethod
+    def _from_hex_char(c):
+        if c >= ord("0") and c <= ord("9"):
+            return c - ord("0")
+        elif c >= ord("A") and c <= ord("F"):
+            return c - "A" + 10
+        elif c >= ord("a") and c <= ord("f"):
+            return c - ord("a") + 10
+        return -1
+
+    @staticmethod
+    def _unescape(input):
+        unescaped = ""
+        i = 0
+        while i < len(input):
+            if input[i] == "%" and i + 2 < len(input):
+                hi = NameComponent._from_hex_char(ord(input[i + 1]))
+                lo = NameComponent._from_hex_char(ord(input[i + 2]))
+
+                if hi < 0 or lo < 0:
+                    unescaped += input[i] + input[i + 1] + input[i + 2]
+                else:
+                    unescaped += chr(hi << 4 | lo)
+                i += 2
+            else:
+                unescaped += input[i]
+            i += 1
+        return unescaped
+
+    @staticmethod
+    def from_escaped_string(input):
+        # Could use urllib only if python3
+
+        # Don't handle . or .. (should be able to construct invalid packets in Scapy)
+        if "=" not in input:
+            return NameComponent(value=NameComponent._unescape(input))
+        else:
+            splitName = input.split("=")
+            # Don't care whether nameType is in valid range
+            nameType = int(splitName[0])
+            return NameComponent(type=nameType,
+                                 value=NameComponent._unescape("".join(input.split("=")[1:])))
+
+            # Handle cases for sha256digest, etc.
+
+    @staticmethod
+    def from_number():
+        pass
+
+    @staticmethod
+    def from_number_with_marker():
+        pass
+
+    @staticmethod
+    def from_version():
+        pass
+
+    @staticmethod
+    def from_timestamp():
+        pass
+
+    @staticmethod
+    def from_sequence_number():
+        pass
+
+    @staticmethod
+    def from_implicit_sha256_digest():
+        pass
+
+    @staticmethod
+    def from_parameters_sha256_digest():
+        pass
+
 # Following two classes given for convenience:
-class ImplicitSha256DC(NameComponent):
+class Sha256Digest(NameComponent):
 
     fields_desc = [
                     NdnTypeField(TYPES['ImplicitSha256DigestComponent']),
-                    NdnLenField("32"),
+                    NdnLenField(default=32),
                     StrFixedLenField("value", "", 32)
                   ]
 
-class ParametersSha256DC(NameComponent):
+class ParamsSha256(NameComponent):
 
     fields_desc = [
                     NdnTypeField(TYPES['ParametersSha256DigestComponent']),
-                    NdnLenField("32"),
+                    NdnLenField(default=32),
                     StrFixedLenField("value", "", 32)
                   ]
 
@@ -112,6 +192,25 @@ class Name(Packet):
                                     length_from=lambda pkt : pkt.length)
                   ]
 
+class CanBePrefix(Packet):
+    name = "CanBePrefix"
+
+    fields_desc = [ NdnTypeField(TYPES['CanBePrefix']) ]
+
+class MustBeFresh(Packet):
+    name = "MustBeFresh"
+
+    fields_desc = [ NdnTypeField(TYPES['MustBeFresh']) ]
+
+class Nonce(Packet):
+    name = "Nonce"
+
+    fields_desc = [
+                    NdnTypeField(TYPES['Nonce']),
+                    NdnLenField(default=4),
+                    XIntField("value", 2)
+                  ]
+
 class Interest(Packet):
     name = "Interest"
     default_name = Name(value=NameComponent(value="test1"))
@@ -121,6 +220,8 @@ class Interest(Packet):
                     NdnLenField(),
                     # InterestName and not name. Otherwise it conflicts
                     # with name field of scapy Packet (base) class
-                    PacketField("InterestName", Name(), Name),
-                    StrLenField("CanBePrefix", "", 1)
+                    PacketField("interestName", Name(), Name),
+                    StrLenField("canBePrefix", "", 1),
+                    StrLenField("mustBeFresh", "", 1),
+                    StrLenField("nonce", "", 1),
                   ]
